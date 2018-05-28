389b9ede47a920f995eed710296aad10bcde01a8
Kamonvan Yingwong <nun.kamonvan150101@hotmail.com>
2018-05-28 13:52:28 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+      <div>
+<body>
+      <div class="wrapper">
+            <nav>
+                <div class="logo">LOGO</div>
+                              <div class="wrapper"></div>
+                              <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                    <li><a class="active" href="#">log in</a></li>
+                              </ul>
+                
+            </nav>
+           <section class="sec 1"></section>
+           <section class="content">
+             <p>somethingttttt-----</p>
+           </section>
+           </div>
+          </body>  
+        
       </div>
+    
     );
   }
 }
